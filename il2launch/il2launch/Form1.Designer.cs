6e2dc003d74f02faeffba0cf2f897f7a3c206443
tfgui <tfgui2@gmail.com>
2019-08-03 12:23:52 +0900

    init

---
+++ il2launch/il2launch/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace il2launch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonTR = new System.Windows.Forms.Button();
+            this.buttonHotas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonTR
+            // 
+            this.buttonTR.Location = new System.Drawing.Point(12, 12);
+            this.buttonTR.Name = "buttonTR";
+            this.buttonTR.Size = new System.Drawing.Size(75, 23);
+            this.buttonTR.TabIndex = 0;
+            this.buttonTR.Text = "TR set";
+            this.buttonTR.UseVisualStyleBackColor = true;
+            this.buttonTR.Click += new System.EventHandler(this.buttonTR_Click);
+            // 
+            // buttonHotas
+            // 
+            this.buttonHotas.Location = new System.Drawing.Point(12, 41);
+            this.buttonHotas.Name = "buttonHotas";
+            this.buttonHotas.Size = new System.Drawing.Size(75, 23);
+            this.buttonHotas.TabIndex = 1;
+            this.buttonHotas.Text = "Hotas";
+            this.buttonHotas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 216);
+            this.Controls.Add(this.buttonHotas);
+            this.Controls.Add(this.buttonTR);
+            this.Name = "Form1";
+            this.Text = "il2 launch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonTR;
+        private System.Windows.Forms.Button buttonHotas;
+    }
+}
+
---
+++ il2launch/il2launch/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Diagnostics;
+
+
+namespace il2launch
+{
+    public partial class Form1 : Form
+    {
+        string keyTR = "dir_tr";
+        string keyTRproc = "proc_tr";
+        string m_dirtr;
+        string m_proctr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitConfig();
+
+            bool res = true;
+            res &= StartDir(m_dirtr);
+
+            if (res)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("some error");
+            }
+        }
+
+        private void InitConfig()
+        {
+            // for TrackIR
+            m_dirtr = GetConfig(keyTR);
+            m_proctr = GetConfig(keyTRproc);
+        }
+
+        private string GetConfig(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string res = appSettings[key] ?? "";
+                if (res == "")
+                {
+                    MessageBox.Show("First Setting " + key);
+                }
+                return res;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+                return "";
+            }
+        }
+
+        private void SetConfig(string key, string value)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool res = CheckProc(m_proctr);
+            if (!res)
+            {
+                Retry("no tr");
+            }
+        }
+
+        private void Retry(string msg)
+        {
+            timer1.Stop();
+
+            DialogResult dr = MessageBox.Show(msg, "press cancel to exit", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.Cancel)
+            {
+                Close();
+            }
+
+            timer1.Start();
+        }
+
+        private bool StartDir(string dirname)
+        {
+            Process.Start(dirname);
+            return true;
+        }
+
+        private bool CheckProc(string procname)
+        {
+            if (procname == "")
+                return false;
+
+            Process[] list = Process.GetProcessesByName(procname);
+            if (list.Length > 0)
+                return true;
+
+            return false;
+        }
+
+        private void buttonTR_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                m_dirtr = openFileDialog1.FileName;
+                m_proctr = openFileDialog1.SafeFileName.Split('.')[0];
+                SetConfig(keyTR, m_dirtr);
+                SetConfig(keyTRproc, m_proctr);
+            }
+        }
+    }
+}
